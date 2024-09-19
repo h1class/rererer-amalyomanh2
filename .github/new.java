65b40983f33f4c108726dc047fa82128d78fb039
amalyomanh1 <123857769+amalyomanh1@users.noreply.github.com>
2024-09-19 13:52:23 +0530

    Create new.java
    Signed-off-by: amalyomanh1 <123857769+amalyomanh1@users.noreply.github.com>

---
+++ .github/new.java
@@ -0,0 +1,11 @@
+public class UserCredentials {
+    public static void main(String[] args) {
+        // Storing email and password
+        String email = "amal@amal.com";
+        String password = "amalpass";
+
+        // Displaying the email and password
+        System.out.println("Email: " + email);
+        System.out.println("Password: " + password);
+    }
+}
